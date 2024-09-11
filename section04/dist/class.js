a265ad4e812a19b30b8b91c358e0694215f1479d
MIN TAE YEONG <dndbxhd10@naver.com>
2024-09-11 10:08:59 +0900

    クラス型を使用したObejctを生成

--- section04/dist/class.js
+++ section04/dist/class.js
@@ -1,30 +1,32 @@
 "use strict";
 // 基本的にクラスの名前は頭文字を大文字にする
+// クラスは型にできる
+// 
 class Person {
     // Objectが作成する時に実行される関数である
     constructor(initName) {
-        // パラメータにthis: {name: string }を作成することで
-        // objectとかで参照する時thisはその位置のnameを指すこととなる
-        // greeting(this: {name: string}) {
-        //     console.log(`Hello My name is ${this.name}`)
-        // }
-        this.greeting = () => {
-            console.log(`Hello My name is ${this.name}`);
-        };
         // thisとはクラスのフィールドが格納されたもの
         this.name = initName;
     }
+    // パラメータにthis: {name: string }を作成することで
+    // objectとかで参照する時thisはその位置のnameを指すこととなる
+    // 普通の関数は呼び出された時に決まる
+    greeting() {
+        console.log(`Hello My name is ${this.name}`);
+    }
 }
+let person2 = new Person("クラス型で宣言");
+person2.greeting();
 const person = new Person("テヨン");
 // 
 person.greeting();
 // console.log("person = ",　person)
-const anotherName = {
-    name: "anotherName",
-    // thisは実行した場所に依存している、つまりどこで使われるかによって変わる
-    anotherGreeting: person.greeting
-};
-// クラスの中のthis.はフィールドを参照するけど
-// anotherGreetingの中のthisはahotherNameというObjectを示している
-// つまりthisは定義された時ではなく呼び出された時決まる
-anotherName.anotherGreeting();
+// const anotherName = {
+//     name: "anotherName",
+//     // thisは実行した場所に依存している、つまりどこで使われるかによって変わる
+//     anotherGreeting: person.greeting
+// }
+// // クラスの中のthis.はフィールドを参照するけど
+// // anotherGreetingの中のthisはahotherNameというObjectを示している
+// // つまりthisは定義された時ではなく呼び出された時決まる
+// anotherName.anotherGreeting();
