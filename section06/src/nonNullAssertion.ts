4745004f5e322cc0ad1ae03fb6939b84dfa0f7e0
MIN TAE YEONG <dndbxhd10@naver.com>
2024-09-12 13:48:12 +0900

    index signature

--- section06/src/nonNullAssertion.ts
+++ section06/src/nonNullAssertion.ts
@@ -1,5 +1,24 @@
-// non null assertion op = !
-// 式の後ろに！マークを付ける
-// 手前の式がnull undefinedではないと言い切る
-// [const input3: HTMLElement]
-const input3 = document.getElementById("input")!
+// index signature
+// 基本的には使わない　使うときは注意すること
+interface Designer {
+    // index signatureを使う場合は
+    // interface内のプロパティの型までindex signatureの型に合わせる必要がある
+    // age: number; // エラー
+    name: string;
+
+    // 括弧の中の型の場合keyの型であって
+    // stringの場合は数字が来てもオッケー
+    // しかしnumberの場合は文字がエラー
+    [index: string]: string
+}
+
+// interfaceに定められているプロパティ以外は付け加えられない
+const designer: Designer = {
+    name: "Quill",
+    role: "dasdawd",
+    job: "musicion"
+}
+
+// index signatureを使った場合下のようにinterfaceに定義されていない
+// プロパティにもアクセスできる
+designer.value
