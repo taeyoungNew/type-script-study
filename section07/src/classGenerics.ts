22fcc42be56cda83af09e3d677253b227208bb86
MIN TAE YEONG <dndbxhd10@naver.com>
2024-09-13 14:31:04 +0900

    use generic class interface

--- section07/src/classGenerics.ts
+++ section07/src/classGenerics.ts
@@ -1,20 +1,5 @@
 // class generics
-// class LightDatabase<T extends string | number | boolean> {
-//     private data: T[] = []
-//     add(item: T) {
-//         this.data.push(item)
-//     }
-
-//     remove(item: T) {
-//         this.data.splice(this.data.indexOf(item), 1)
-//     }
-
-//     get() {
-//         return this. data
-//     }
-// }
-
-class LightDatabase {
+class LightDatabase<T extends string | number | boolean> {
     private data: T[] = []
     add(item: T) {
         this.data.push(item)
@@ -27,6 +12,17 @@ class LightDatabase {
     get() {
         return this. data
     }
+}
+
+// interface
+interface TmpDatabase<T> {
+    id: number;
+    data: T[]
+}
+
+const tempDatabase: TmpDatabase<number> = {
+    id: 3,
+    data: [32]
 }
 
 const strLightDatabase = new LightDatabase<string>()
