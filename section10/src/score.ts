6e9d1ad4745ebcac07f90fc946228291d6429a7c
taeyeongNew <dndbxhd10@naver.com>
2024-09-14 23:34:12 +0900

    TS module commonJS

---
+++ section10/src/score.ts
@@ -0,0 +1,25 @@
+import { Scoreable } from "./interfaces.js";
+import { Foods } from "./foods.js";
+export class Score implements Scoreable {
+
+    private static instance: Score;
+    private constructor() {
+    }
+    static getInstance() {
+        if(!Score.instance) {
+            Score.instance = new Score()
+        }
+
+        return Score.instance
+    }
+    get totalScore() {
+        const foods = Foods.getInstance()
+        console.log(foods)
+        return foods.activeElsScore.reduce((total, score) => total + score, 0)
+    }
+
+    render() {
+        document.querySelector(".score__number")!.textContent = String(this.totalScore)
+    }
+
+}
